c6ba713cd71d8279c8a378a0e92dd4825eb7f963
amir <kmuc72@gmail.com>
2019-03-23 14:13:06 +0100

    payments

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Query } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentParams } from 'src/shared/params/payment.params';
+import { ListPaymentParams } from 'src/shared/params/listPayment.params';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentService: PaymentsService) {}
 
   @Post()
-  async getMonetaryAccounts(@Body() userId: PaymentParams): Promise<any> {
+  async makePayment(@Body() userId: PaymentParams): Promise<any> {
     return await this.paymentService.makePayment(userId);
+  }
+
+  @Get()
+  async getPayment(@Query() userId: ListPaymentParams): Promise<any> {
+    return await this.paymentService.getPayment(userId);
   }
 }
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { BunqClientService } from 'src/bunq-client/bunq-client.service';
 import { PaymentParams } from 'src/shared/params/payment.params';
+import { ListPaymentParams } from 'src/shared/params/listPayment.params';
 
 @Injectable()
 export class PaymentsService {
+  async getPayment(userId: ListPaymentParams): Promise<any> {
+    return await this.authService
+      .getBunqClient()
+      .then(async bunqclient => {
+        let accounts = await bunqclient.api.payment.list(
+          userId.userId,
+          userId.monetaryAccountId,
+        );
+
+        return accounts;
+      })
+      .catch(error => {
+        console.log(error.response.data);
+      });
+  }
   constructor(private readonly authService: BunqClientService) {}
 
   async makePayment(userId: PaymentParams): Promise<any> {
